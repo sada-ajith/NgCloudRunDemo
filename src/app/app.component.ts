0b27404ab46c540dc13e68e78f3a4a67a05676d3
Ajith Padmakumar <ajith.padmakumar@sada.com>
2023-07-27 18:41:23 +0530

    Updated lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+export interface Ace {
+  attr : string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngcloudrundemo';
+  ANANANA = '';
+  func() {
+    return 
+  }
 }
